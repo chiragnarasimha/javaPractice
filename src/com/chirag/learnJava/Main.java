d11942401dd119409f1b64f694c57b9d4988802d
Chirag Narasimha Murthy <c.narasimha.murthy@accenture.com>
2021-05-14 12:23:42 +1000

    sum3And5Challenge() done

--- src/com/chirag/learnJava/Main.java
+++ src/com/chirag/learnJava/Main.java
@@ -3,14 +3,16 @@ package com.chirag.learnJava;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
         printVariables();
         challenge1();
-        challenge2(5000);
+        challenge2(200);
+        sum3And5Challenge();
 
     }
 
     public static void printVariables() {
+        System.out.println("From printVariables()");
         int myValue = 1000;
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MAX_VALUE);
@@ -23,14 +25,24 @@ public class Main {
         long testLong = 2_147_483_647_123L;
         System.out.println(testLong);
 
-        int minInt = Integer.MIN_VALUE+100/2;
-        byte testByteValue = (byte)(minInt/2);
-        System.out.println("byte = "+testByteValue);
+        int minInt = Integer.MIN_VALUE + 100 / 2;
+        byte testByteValue = (byte) (minInt / 2);
+        System.out.println("byte = " + testByteValue);
 
         float maxFloat = Float.MAX_VALUE;
         float minFloat = Float.MIN_VALUE;
 
-        System.out.println("maxFloat = " + maxFloat + " | minFloat = " +minFloat );
+        System.out.println("maxFloat = " + maxFloat + " | minFloat = " + minFloat);
+        System.out.println("Add String to Int = " + (Integer.valueOf("20") + 10));
+
+        boolean isCar = true;
+        if (!isCar) {
+            System.out.println("The item is not a car");
+        }
+
+        // The same thing can be accomplished using ternary operators
+        String wasCar = isCar ? "Yes, it was a car" : "No, it wasnt a car";
+        System.out.println(wasCar);
     }
 
     public static void challenge1() {
@@ -39,9 +51,13 @@ public class Main {
         short shortNum = Short.MAX_VALUE - 100;
 //        System.out.println(shortNum);
         int intNum = 50000;
-        long longNum = (long)(intNum + 10*(byteNum+shortNum));
+        long longNum = (long) (intNum + 10 * (byteNum + shortNum));
         System.out.println("longNum = " + longNum);
 
+        char myChar = 'D';
+        char myCharUnicode = '\u0044';
+        System.out.println("");
+        System.out.println("Challenge 1:  " + "myChar = " + myChar + " | myCharUnicode = " + myCharUnicode);
     }
 
     /**
@@ -50,7 +66,7 @@ public class Main {
      *
      * <p>
      * ### Inputs : poundsInput
-     *    1. poundsInput - number in grams
+     * 1. poundsInput - number in grams
      *
      * <p>
      * <table border="1" cellpadding="5px" style="text-align:center">
@@ -59,7 +75,34 @@ public class Main {
      * | 13 May 2021  | Chirag Narasimha Murthy  | Created       |
      * </table>
      */
-    public static void challenge2(int poundsInput){
-        System.out.println(poundsInput + "g in Kg = " +  (double)poundsInput * 0.45359237 + "Kg");
+    public static void challenge2(int poundsInput) {
+        System.out.println("");
+        System.out.println("Challenge 2:  " + poundsInput + "g in Kg = " + (double) poundsInput * 0.45359237d + "Kg");
     }
+
+    public static void sum3And5Challenge() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("This is from the sum3And5Challenge()");
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= 1000; i++) {
+
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                sum += i;
+                System.out.println("Can be divided by both 3 and 5 ->> "+i+" | Sum = " +sum);
+                count++;
+            }
+            if (count == 5) {
+                break;
+                System.out.println("We found 5 numbers already");
+            }
+            
+        }
+
+    }
+
+
 }
