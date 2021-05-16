524e3ff660a788871f027b0c7cb7aeedad3d28f7
Chirag Narasimha Murthy <c.narasimha.murthy@accenture.com>
2021-05-16 19:25:19 +1000

    Cleaned up the Main.java class

--- src/com/chirag/learnJava/Main.java
+++ src/com/chirag/learnJava/Main.java
@@ -5,36 +5,8 @@ import static com.chirag.learnJava.javaBasics.getBucketCount;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-//        printVariables();
-//        challenge1();
-//        challenge2(200);
-//        sum3And5Challenge();
+
         long start;
-////        System.out.println("isPalindromeChirag = " + isPalindromeChirag(-1221));
-//        System.out.println("isPalindromeChirag = " + isPalindromeChirag(1221));
-//        timeTakenToRun(start);
-//
-//        start = System.nanoTime();
-////        System.out.println("isPalindromeChirag = " + isPalindromeChirag(-1221));
-//        System.out.println("isPalindromeUdemy = " + isPalindromeUdemy(1221));
-//        timeTakenToRun(start);
-
-//
-//        start = System.nanoTime();
-//        System.out.println(sumFirstAndLastDigit(0));
-//        timeTakenToRun(start);
-
-//        start = System.nanoTime();
-//        System.out.println(getEvenDigitSum(0));
-//        timeTakenToRun(start);
-//        start = System.nanoTime();
-//        System.out.println(hasSharedDigitChirag(12, 13));
-//        timeTakenToRun(start);
-//        start = System.nanoTime();
-//
-//        System.out.println(hasSharedDigitUdemy(12, 13));
-//        timeTakenToRun(start);
 
         double sum = 0;
         int numberOfTimesToRun = 1;
@@ -42,24 +14,11 @@ public class Main {
 //        for (int i = 0; i < numberOfTimesToRun; i++) {
 //            System.out.println("");
 //            start = System.nanoTime();
-////            System.out.println(hasSharedDigitChirag(12, 13));
-////            System.out.println(hasSharedDigitUdemy(15, 13));
-////            System.out.println((hasSameLastDigit(10, 10, 1000)));
-////            System.out.println(numberToWords(123));
-////            numberToWords(100);
-////            System.out.println(canPack(1, 1, 11));
-//            printSquareStar(-5);
+
 //            sum += timeTakenToRun(start);
 //        }
 //        System.out.println("Average time to run = " + (sum / (double) numberOfTimesToRun) + "ms");
 
-//        scanUserInput();
-//        sumAllUserInputsChirag(5);
-//        sumAllUserInputsTim(5);
-//        minMaxChallenge(5);
-//        inputThenPrintSumAndAverage();
     }
-
-
 
 }
